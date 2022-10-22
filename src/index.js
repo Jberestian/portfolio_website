69324b9a9b4ffc7eef309f413ecafc080ff502be
Yevhen <jberestian@gmail.com>
2022-10-22 11:02:04 +0300

    section functionality

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-const section = document.querySelectorAll('.section');
+const sections = document.querySelectorAll('.section');
 const sectBtns = document.querySelectorAll('.controlls');
 const sectBtn = document.querySelectorAll('.control');
 const allSections = document.querySelector('.main-content');
@@ -15,6 +15,24 @@ function pageTransitions() {
       this.className += ' active-btn';
     });
   }
+  //Sections active class
+  allSections.addEventListener('click', e => {
+    const id = e.target.dataset.id;
+    if (id) {
+      //removu selected from the other button
+      sectBtns.forEach(btn => {
+        btn.classList.remove('active');
+      });
+      e.target.classList.add('active');
+
+      //hide other sections
+      sections.forEach(section => {
+        section.classList.remove('active');
+      });
+      const element = document.getElementById(id);
+      element.classList.add('active');
+    }
+  });
 }
 
 pageTransitions();
